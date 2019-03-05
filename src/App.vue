47a52e19eb7339a506b245d4d03d7eee5d29702f
andrew_r <andrew.r224@gmail.com>
2019-03-05 15:14:44 +0200

    Remove clutter and install dependencies :chicken:

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+      <router-link to="/">list</router-link>
+    </nav>
+
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from "./components/List"
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    List
   }
 }
 </script>
 
 <style>
+/* color: #D62839 */
+/* color: #BA324F */
+/* color: #175676 */
+/* color: #4BA3C3 */
+/* color: #CCE6F4 */
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #CCE6F4 ;
   margin-top: 60px;
 }
 </style>
